60c4a3a18808afd8da4d0ae9abe5b1f4a328e132
System Administrator <root@dawang.local>
2018-05-10 15:54:54 +0800

    commit

--- battle-dao/src/main/java/com/battle/dao/UserStatusDao.java
+++ battle-dao/src/main/java/com/battle/dao/UserStatusDao.java
@@ -2,6 +2,7 @@ package com.battle.dao;
 
 import javax.persistence.LockModeType;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Lock;
@@ -11,6 +12,8 @@ import com.battle.domain.UserStatus;
 
 public interface UserStatusDao extends CrudRepository<UserStatus, String>{
 
+	
+	@Cacheable(value="userCache") 
 	@Lock(value=LockModeType.PESSIMISTIC_WRITE)
 	UserStatus findOne(String id);
 
