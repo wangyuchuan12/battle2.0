f85749770ddbbc42ba141a551e481923c0e49bf8
System Administrator <root@dawang.local>
2018-05-10 16:00:35 +0800

    commit

--- battle-models/src/main/java/com/battle/domain/UserStatus.java
+++ battle-models/src/main/java/com/battle/domain/UserStatus.java
@@ -1,5 +1,7 @@
 package com.battle.domain;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -16,7 +18,12 @@ import com.wyc.annotation.ParamEntityAnnotation;
 @Entity
 @Table(name="user_status")
 @ParamEntityAnnotation
-public class UserStatus {
+public class UserStatus implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@IdAnnotation
 	private String id;
